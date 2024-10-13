83e9d85db6cc2af32e3e9e6216d7b486675713c6
Jani Pekkala <103809987+Jaspak1778@users.noreply.github.com>
2024-10-13 20:34:20 +0300

    Update settings.py

--- kauppatoriapp/settings.py
+++ kauppatoriapp/settings.py
@@ -21,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-yo=0$rk-yjrb-@unl!kc5*!#)xq-glww)s$34vs3pzexz6o6++'
+SECRET_KEY = 
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
